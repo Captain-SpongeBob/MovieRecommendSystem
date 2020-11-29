2218c5a04861485101dd32dac3b8ba3591318844
Captain-SpongeBob <973280963@qq.com>
2020-11-29 22:01:59 +0800

    //

--- recommender/OfflineRecommender/src/main/scala/com/lds/offline/OfflineRecommender.scala
+++ recommender/OfflineRecommender/src/main/scala/com/lds/offline/OfflineRecommender.scala
@@ -2,141 +2,140 @@ package com.lds.offline
 
 import org.apache.spark.SparkConf
 import org.apache.spark.mllib.recommendation.{ALS, Rating}
-import org.apache.spark.rdd.RDD
-import org.apache.spark.sql.{DataFrame, Dataset, SparkSession}
+import org.apache.spark.sql.SparkSession
 import org.jblas.DoubleMatrix
 
-case class Movie(mid: Int, name: String, descri: String, timelong: String, issue: String,
-                 shoot: String, language: String, genres: String, actors: String, directors: String)
-case class MoiveRating(uid: Int, mid: Int, score: Double, timestamp: Int)
-case class MongoConfig(uri: String, db: String)
-//标准的推荐对象
-case class Recommendation(mid: Int, score: Double)
-//用户推荐对象
-case class UserRecs(uid: Int, seq: Seq[Recommendation])
-//电影相似度(与该电影相似的电影列表）
-case class MovieRecs(mid: Int, recs: Seq[Recommendation])
+// 基于评分数据的LFM，只需要rating数据
+case class MovieRating(uid: Int, mid: Int, score: Double, timestamp: Int )
+
+case class MongoConfig(uri:String, db:String)
+
+// 定义一个基准推荐对象
+case class Recommendation( mid: Int, score: Double )
+case class Movie(mid: Int, name: String, descri: String, timelong: String,
+                 issue: String, shoot: String,language: String, genres: String, actors: String, directors: String)
+// 定义基于预测评分的用户推荐列表
+case class UserRecs( uid: Int, recs: Seq[Recommendation] )
+
+// 定义基于LFM电影特征向量的电影相似度列表
+case class MovieRecs( mid: Int, recs: Seq[Recommendation] )
+
 object OfflineRecommender {
 
-
-  // 定义常量val
+  // 定义表名和常量
   val MONGODB_RATING_COLLECTION = "Rating"
-  val MONGODB_MOVIE_COLLECTION = "Movie"
-  // 推荐表的名称
-  val USER_RECS= "UserRecs"
-  val MOVIE_RECS= "MovieRecs"
-  val USER_MAX_RECOMMENDATION= 20
-  // 定义配置
-  val config = Map(
-    "spark.cores" -> "local[*]",
-    "mongo.uri" -> "mongodb://localhost:27017/recommender_movie",
-    "mongo.db" -> "recommender_movie")
+  val MONGODB_MOVIE_COLLECTION= "Movie"
+  val USER_RECS = "UserRecs"
+  val MOVIE_RECS = "MovieRecs"
+
+  val USER_MAX_RECOMMENDATION = 20
+
   def main(args: Array[String]): Unit = {
+    val config = Map(
+      "spark.cores" -> "local[*]",
+      "mongo.uri" -> "mongodb://localhost:27017/recommender_movie",
+      "mongo.db" -> "recommender_movie"
+    )
+
     val sparkConf = new SparkConf().setMaster(config("spark.cores")).setAppName("OfflineRecommender")
+
+    // 创建一个SparkSession
     val spark = SparkSession.builder().config(sparkConf).getOrCreate()
-    implicit val mongoConfig = MongoConfig(config("mongo.uri"),config("mongo.db"))
+
     import spark.implicits._
 
-    /**
-     * 评分的数据集
-     * Rating(uid: Int, mid: Int, score: Double, timestamp: Int)
-     */
-    val ratingDS: Dataset[(Int, Int, Double)] = spark
-      .read
-      .option("uri",mongoConfig.uri)
-      .option("collection",MONGODB_RATING_COLLECTION )
+    implicit val mongoConfig = MongoConfig(config("mongo.uri"), config("mongo.db"))
+
+
+    // 加载数据
+    val ratingRDD = spark.read
+      .option("uri", mongoConfig.uri)
+      .option("collection", MONGODB_RATING_COLLECTION)
       .format("com.mongodb.spark.sql")
       .load()
-      .as[MoiveRating]
-      .map(rating => (rating.uid, rating.mid, rating.score))
+      .as[MovieRating]
+      .rdd
+      .map( rating => ( rating.uid, rating.mid, rating.score ) )    // 转化成rdd，并且去掉时间戳
       .cache()
-    // 用户的数据集
-    val userDS: Dataset[Int] = ratingDS.map(_._1).distinct()
-    /**
-     * 电影的数据集
-     * 、Movie(
-     *    mid: Int, name: String, descri: String, timelong: String,issue: String,
-     *    shoot: String, language: String, genres:String, actors: String,directors: String
-     *    )
-     */
-    val movieDS = spark
-      .read
-      .option("uri",mongoConfig.uri)
-      .option("collection",MONGODB_MOVIE_COLLECTION )
+
+    // 从rating数据中提取所有的uid和mid，并去重
+    val userRDD = ratingRDD.map(_._1).distinct()
+
+    val movieRDD =  spark.read
+      .option("uri", mongoConfig.uri)
+      .option("collection", MONGODB_MOVIE_COLLECTION)
       .format("com.mongodb.spark.sql")
       .load()
       .as[Movie]
-      .map(_.mid)
+      .rdd
+      .map(_.mid)    // 转化成rdd，并且去掉时间戳
       .cache()
-    //创建训练数据集
-    val trainData: RDD[Rating] = ratingDS.map(x => Rating(x._1, x._2, x._3)).rdd
-    //rank 是模型中隐语义因子的个数, iterations 是迭代的次数, lambda 是ALS的正则化参
-    val (rank,iterations,lambda) = (50, 5, 0.01)
-    //调用ALS算法训练隐语义模型
-    val model = ALS.train(trainData,rank,iterations,lambda)
-    //计算用户推荐矩阵
-    val userMovies= userDS.rdd.cartesian(movieDS.rdd)
-    // model已训练好，把id传进去就可以得到预测评分列表RDD[Rating] (uid,mid,rating)
-    val preRatings: RDD[Rating] = model.predict(userMovies)
-    val userRecs: DataFrame = preRatings
-      .filter(_.rating > 0)
-      .map(rating => (rating.user, (rating.product, rating.rating)))
+
+    // 训练隐语义模型
+    val trainData = ratingRDD.map( x => Rating(x._1, x._2, x._3) )
+
+    val (rank, iterations, lambda) = (200, 5, 0.1)
+    val model = ALS.train(trainData, rank, iterations, lambda)
+
+    // 基于用户和电影的隐特征，计算预测评分，得到用户的推荐列表
+    // 计算user和movie的笛卡尔积，得到一个空评分矩阵
+    val userMovies = userRDD.cartesian(movieRDD)
+
+    // 调用model的predict方法预测评分
+    val preRatings = model.predict(userMovies)
+
+    val userRecs = preRatings
+      .filter(_.rating > 0)    // 过滤出评分大于0的项
+      .map(rating => ( rating.user, (rating.product, rating.rating) ) )
       .groupByKey()
       .map{
-        case (uid, recs) => UserRecs(uid, recs
-          .toList.sortWith(_._2 > _._2)
-          .take(USER_MAX_RECOMMENDATION)
-          .map(x => Recommendation(x._1,x._2)))
+        case (uid, recs) => UserRecs( uid, recs.toList.sortWith(_._2>_._2).take(USER_MAX_RECOMMENDATION).map(x=>Recommendation(x._1, x._2)) )
       }
       .toDF()
 
-    userRecs
-      .write
-      .option("uri",mongoConfig.uri)
-      .option("collection",USER_RECS)
-      .mode("overwrite")
-      .format("com.mongodb.spark.sql")
-      .save()
-
-    //TODO：计算电影相似度矩阵
-    //计算电影相似度矩阵//获取电影的特征矩阵，
-    // 数据格式RDD[(scala.Int, scala.Array[scala.Double])]
-    val movieFeatures: RDD[(Int, DoubleMatrix)] = model.productFeatures.map{
-      case (mid,features) =>(mid, new DoubleMatrix(features))
-    }
-    // 计算笛卡尔积并过滤合并
-    val movieRecs = movieFeatures
-      .cartesian(movieFeatures)
-      .filter{
-        case (a,b) => a._1 != b._1
-      }
-      .map{
-        case (a,b) =>
-          val simScore = this.consinSim(a._2,b._2)
-          // 求余弦相似度
-          (a._1,(b._1,simScore))
-      }
-      .filter(_._2._2 > 0.6)
-      .groupByKey()
-      .map{
-        case (mid,items) => MovieRecs(mid,items.toList.map(x => Recommendation(x._1,x._2)))
-      }
-      .toDF()
-
-    movieRecs
-      .write
+    userRecs.write
       .option("uri", mongoConfig.uri)
-      .option("collection",MOVIE_RECS)
+      .option("collection", USER_RECS)
       .mode("overwrite")
       .format("com.mongodb.spark.sql")
       .save()
 
+    // 基于电影隐特征，计算相似度矩阵，得到电影的相似度列表
+    val movieFeatures = model.productFeatures.map{
+      case (mid, features) => (mid, new DoubleMatrix(features))
+    }
 
+    // 对所有电影两两计算它们的相似度，先做笛卡尔积
+    val movieRecs = movieFeatures.cartesian(movieFeatures)
+      .filter{
+        // 把自己跟自己的配对过滤掉
+        case (a, b) => a._1 != b._1
+      }
+      .map{
+        case (a, b) => {
+          val simScore = this.consinSim(a._2, b._2)
+          ( a._1, ( b._1, simScore ) )
+        }
+      }
+      .filter(_._2._2 > 0.8)    // 过滤出相似度大于0.6的
+      .groupByKey()
+      .map{
+        case (mid, items) => MovieRecs( mid, items.toList.sortWith(_._2 > _._2).take(USER_MAX_RECOMMENDATION).map(x => Recommendation(x._1, x._2)) )
+      }
+      .toDF()
+    movieRecs.write
+      .option("uri", mongoConfig.uri)
+      .option("collection", MOVIE_RECS)
+      .mode("overwrite")
+      .format("com.mongodb.spark.sql")
+      .save()
 
-    // 关闭sparkspark.stop()
+    spark.stop()
   }
-  //计算两个电影之间的余弦相似度
-  def consinSim(movie1: DoubleMatrix, movie2:DoubleMatrix) : Double ={
-    movie1.dot(movie2) / ( movie1.norm2()  * movie2.norm2() )
+
+  // 求向量余弦相似度
+  def consinSim(movie1: DoubleMatrix, movie2: DoubleMatrix):Double ={
+    movie1.dot(movie2) / ( movie1.norm2() * movie2.norm2() )
   }
+
 }
